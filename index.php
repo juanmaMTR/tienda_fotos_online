0d428fcbd069db18db74b12b954449b281e5563e
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-01-31 10:15:37 +0100

    Add somethings

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>">
+        <link rel="stylesheet" href="src/css/style.css">
+        <title>Tienda Fotos Online</title>
+    </head>
+    <body>
+        <h1>Tienda de Fotos Online</h1>
+        <form action="" method="post">
+            
+        </form>
+    </body>
+</html>
